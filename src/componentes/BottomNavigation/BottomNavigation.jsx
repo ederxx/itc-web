42c7088ce87e2d871e67bbbe251c025bbe5734f5
ederxx <93275824+ederxx@users.noreply.github.com>
2025-05-13 14:43:18 -0300

    Update BottomNavigation.jsx
    //

--- src/componentes/BottomNavigation/BottomNavigation.jsx
+++ src/componentes/BottomNavigation/BottomNavigation.jsx
@@ -5,10 +5,10 @@ import './BottomNavigation.css';
 export default function BottomNavigation() {
   return (
     <nav className="bottom-nav">
-      <a href="#">HOME</a>
-      <a href="#">QUEM SOMOS</a>
-      <a href="#">MISSÃO</a>
-      <a href="#">GLOSSÁRIO</a>
+      <a href="/">HOME</a>
+      <a href="/">QUEM SOMOS</a>
+      <a href="/">MISSÃO</a>
+      <a href="/">GLOSSÁRIO</a>
     </nav>
   );
 }
